4e65c1fd059e7f43f16439958b6f06ba9a55f108
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-04-27 14:11:56 -0300

    alterando proxy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-const baseUrl = "https://vila-bet-api.herokuapp.com/";
+const baseUrl = "http://localhost:3000/";
 
 const request = async (method, endpoint, params, token = null) => {
   method = method.toLowerCase();
